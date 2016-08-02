7fe98dee4d3ba69ad4928639cccd47f570293643
HasiniWijerathna <hasini@calcey.com>
2016-08-02 00:05:02 -0700

    created getters and setters for roidata

--- app/providers/data-service/data-service.ts
+++ app/providers/data-service/data-service.ts
@@ -7,7 +7,29 @@ export class DataService {
   roiData: ROIData;
 
   constructor() {
-    this.roiData = null;
+    this.initEmptyROIData();
+  }
+
+  // Creates an empty ROI data object in the memory.
+  private initEmptyROIData(): void {
+    let roiData = new ROIData();
+    this.roiData = roiData;
+  }
+
+  /**
+   * Returns the current ROI Data in the memory.
+   * @return {ROIData} Current ROI data.
+   */
+  public getROIData(): ROIData {
+    return this.roiData;
+  }
+
+  /**
+   * Saves the given ROI data object in the memory.
+   * @param {ROIData} roiData The ROI data to be saved.
+   */
+  public setROIData(roiData: ROIData): void {
+    this.roiData = roiData;
   }
 
 }
