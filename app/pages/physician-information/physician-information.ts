5783c6337c4dc8183b1a898832a3994c42c417c4
HasiniWijerathna <hasini@calcey.com>
2016-08-25 16:41:46 +0530

    created physician information page

---
+++ app/pages/physician-information/physician-information.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Control, ControlGroup } from '@angular/common';
+import { NavController } from 'ionic-angular';
+
+import { emailAddressValidator, passwordValidator, isIntegerValidator } from '../../validators/custom-validators';
+
+
+@Component({
+  templateUrl: 'build/pages/physician-information/physician-information.html',
+})
+export class PhysicianInformationPageComponent {
+
+  private navController = NavController;
+  private facilityForm: ControlGroup;
+  constructor(navController: NavController) {
+
+    this.facilityForm = new ControlGroup({
+      email: new Control('email', emailAddressValidator),
+      integer: new Control('integer', isIntegerValidator),
+      password: new Control('password', passwordValidator)
+    });
+  }
+}
