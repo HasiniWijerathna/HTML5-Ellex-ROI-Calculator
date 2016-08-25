f43d76a997c662d52453106efb53a6e7a17b0b5b
HasiniWijerathna <hasini@calcey.com>
2016-08-25 16:45:13 +0530

    implemented the total finance calculation

---
+++ app/models/roi-data/roi-data.spec.ts
@@ -0,0 +1,10 @@
+import { describe, expect, it } from '@angular/core/testing';
+
+describe('roiValidator', () => {
+
+  it('should calculate', () => {
+    expect((2 + 2) * 5 * 5).toBe(100);
+    expect((4 + 4) * 10 * 10).toBe(800);
+  });
+
+});
